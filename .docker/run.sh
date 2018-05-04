91cd10bc954326ec86d461b405cbc6bb4124ac01
Brian Torres-Gil <btorres-gil@paloaltonetworks.com>
2018-05-03 22:12:19 -0700

    Significant enhancements across modules

--- .docker/run.sh
+++ .docker/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d --name jupyter-notebook -p 80:8888 --restart unless-stopped -v /home/ubuntu/pandevice:/home/jovyan/work -w /home/jovyan/work jupyter-notebook start-notebook.sh --NotebookApp.token=''
+docker run -d --name jupyter-notebook -p 80:8888 --restart unless-stopped -v /home/ubuntu/pandevice:/home/jovyan/work -w /home/jovyan/work jupyter-notebook start-notebook.sh --NotebookApp.token='' --ContentsManager.allow_hidden=True
